212b08f1c7613cde008a0689f9cdf2e00b3e11fc
Melksedeque de Souza Silva <freelancer@melksedeque.com.br>
2021-10-14 20:11:45 -0300

    Atualização na formatação da data

--- Data.php
+++ Data.php
@@ -3,11 +3,11 @@
 $data = new DateTime();
 $intervalo = new DateInterval('P5DT10H50M');
 
-echo $data->format('d/m/Y H:i:s');
+echo $data->format('d/m/Y - H:i:s');
 echo '<br><br>';
 
 $data->sub($intervalo);
 
-echo $data->format('d/m/Y H:i:s');
+echo $data->format('d/m/Y - H:i:s');
 
 ?>
